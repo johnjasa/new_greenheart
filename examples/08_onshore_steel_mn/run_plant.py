7f90918cb026c9530cedbeaa5c5972b023b3c1b5
bayc <christopher.j.bay@gmail.com>
2025-02-19 22:21:00 -0700

    organize example 08 input files

--- examples/08_onshore_steel_mn/run_plant.py
+++ examples/08_onshore_steel_mn/run_plant.py
@@ -2,7 +2,7 @@ from new_greenheart.core.greenheart_model import GreenHEARTModel
 
 
 # Create a GreenHEART model
-gh = GreenHEARTModel('hybrid_steel.yaml')
+gh = GreenHEARTModel('08_onshore_steel_mn.yaml')
 
 # Run the model
 gh.run()
