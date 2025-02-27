5923261e732f8d5070698815c32662a11dc1a8fa
kbrunik <kbrunik@gmail.com>
2025-02-27 10:53:43 -0700

    update pysam wind and solar to not require api call for resource

--- new_greenheart/converters/solar/solar_pysam.py
+++ new_greenheart/converters/solar/solar_pysam.py
@@ -12,25 +12,34 @@ from new_greenheart.core.utilities import BaseConfig
 
 @define
 class PYSAMSolarPlantPerformanceComponentConfig(BaseConfig):
-    lat: float = field()
-    lon: float = field()
+    """Configuration class for the location of the solar pv plant PYSAMSolarPlantPerformanceComponentSite.
+
+    Args:
+        latitude (float): Latitude of wind plant location.
+        longitude (float): Longitude of wind plant location.
+        year (float): Year for resource.
+        solar_resource_filepath (str): Path to solar resource file. Defaults to "".
+    """
+    latitude: float = field()
+    longitude: float = field()
     year: float = field()
+    solar_resource_filepath: str = field(default="")
 
 
 class PYSAMSolarPlantPerformanceComponent(SolarPerformanceBaseClass):
     """
     An OpenMDAO component that wraps a SolarPlant model.
-    It takes wind parameters as input and outputs power generation data.
+    It takes solar parameters as input and outputs power generation data.
     """
     def setup(self):
         super().setup()
-        self.config = PYSAMSolarPlantPerformanceComponentConfig(
+        self.config = PYSAMSolarPlantPerformanceComponentConfig.from_dict(
             self.options["plant_config"]["site"]
         )
         self.config_name = "PVWattsSingleOwner"
         self.system_model = Pvwatts.default(self.config_name)
 
-        solar_resource = SolarResource(self.config.lat, self.config.lon, self.config.year)
+        solar_resource = SolarResource(lat=self.config.latitude, lon=self.config.longitude, year=self.config.year,filepath=self.config.solar_resource_filepath)
         self.system_model.value("solar_resource_data", solar_resource.data)
 
     def compute(self, inputs, outputs):
